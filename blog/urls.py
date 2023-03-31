1b6062d68fc05958470a5549e2d2a19a34e7935a
valesha <93388853+valesha@users.noreply.github.com>
2023-03-31 09:41:43 +0300

    Add files via upload

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView
+)
+
+urlpatterns = [
+    path("post/new/", BlogCreateView.as_view(), name="post_new"), # new
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"), # new
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"), # new
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"), # new
+    path("", BlogListView.as_view(), name="home"),
+]
---
+++ blog/views.py
@@ -0,0 +1,35 @@
+from django.views.generic import ListView, DetailView # new
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView  # new
+from django.urls import reverse_lazy, reverse  # new
+
+
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
